4395aa5f7c501fcbf752a6949f5f09586a0ea451
thodotpro <116018129+thodotpro@users.noreply.github.com>
2024-11-27 22:13:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+article = {1: ['schuhe', 100], 2: ['iphone', 1000], 3: ['ipad', 800], 3: ['airpods', 300], 4: ['kugelschreiber', 2]}
+
+order_number = 1
+shopping_cart = []
+
+
+class shopping:
+
+    def __init__(self):
+        self.shopping_cart = cart
+        self.fill_cart = fill
+
+    def cart():
+        shopping_cart = []
+        while order_number >= 0:
+            shopping_cart = []
+            print(shopping_cart)
+            break
+
+    def fill_cart(*kwargs):
+        shopping_cart.append(kwargs)
+        print(shopping_cart)
+
+
+shopping.cart()
+shopping.fill_cart(article[1], article[1], article[2], article[3])
