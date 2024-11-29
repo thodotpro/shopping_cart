8195519ede7f1dd6238bb918c8e4f83b2260ff94
thodotpro <tho.pro@posteo.org>
2024-11-29 19:55:33 +0100

    t branch checkout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-article = {1: ['schuhe', 100], 2: ['iphone', 1000], 3: ['ipad', 800], 3: ['airpods', 300], 4: ['kugelschreiber', 2]}
+article = {1: ['schuhe', 100], 2: ['iphone', 1000], 3: ['ipad', 800], 4: ['airpods', 300], 5: ['kugelschreiber', 2]}
+
 
 order_number = 1
 shopping_cart = []
@@ -7,20 +8,27 @@ shopping_cart = []
 class shopping:
 
     def __init__(self):
+        self.article = article
         self.shopping_cart = cart
         self.fill_cart = fill
 
-    def cart():
-        shopping_cart = []
-        while order_number >= 0:
-            shopping_cart = []
-            print(shopping_cart)
-            break
+    def available():
+        print("-------- Available --------")
+        for key, value in article.items():
+            print(f"{key} - {value[0]:15}{value[1]:10.2f} €")
+        print("---------- END -----------")
 
-    def fill_cart(*kwargs):
-        shopping_cart.append(kwargs)
-        print(shopping_cart)
+    # def fill_cart(**kwargs):
+    #     print()
+    #     print("--Fill cart with available items--")
+    #     active_shop = True
+    #     while active_shop:
+    #         current_cart = input("Enter article you wish to buy (q to quit/c to cancel): ")
+    #         if current_cart:
+    #             shopping_cart.append(current_cart)
+    #         elif current_cart.lower() == "q":
+    #             active_shop = False
 
 
-shopping.cart()
-shopping.fill_cart(article[1], article[1], article[2], article[3])
+shopping.available()
+# shopping.fill_cart()
