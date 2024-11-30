b3717639e1d46600ecffd1739bd88b0ba7a45c16
thodotpro <tho.pro@posteo.org>
2024-11-30 13:32:39 +0100

    fill cart

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-articles = {1: ['schuhe', 100], 2: ['iphone', 1000], 3: ['ipad', 800], 4: ['airpods', 300], 5: ['kugelschreiber', 2]}
+from astropy.cosmology import available
+
+articles = {"1": ['schuhe', 100], "2": ['iphone', 1000], "3": ['ipad', 800], "4": ['airpods', 300], "5": ['kugelschreiber', 2]}
 prices = {1: '100', 2: '1000', 3: '800', 4: '300', 5: '2'}
 
 order_number = 1
-shopping_cart = []
+shopping_cart = ['1']
 
 class shopping:
 
     def __init__(self):
         self.article = articles
-        self.shopping_cart = cart
-        self.fill_cart = fill
+        self.available = available
+        self.fill_cart = fill_cart
 
     # Funktion listet die verfügbaren Artikel auf
     def available():
@@ -19,32 +21,36 @@ class shopping:
         print("---------- END -----------")
 
 
-    # Eingabefehler funktionieren noch nicht ganz. Value Error mit int und str
+    # Funktion füllt Einkaufswagen nach User Input der value keys
     def fill_cart():
         print()
         print("--Fill cart with available items--")
         active_shop = True
         exit = "q"
         complete = "c"
+        current_order = []
         while active_shop:
             order = input("Enter article no. you want to add (q to quit - c to complete): ")
-            if order == exit.lower():
+            if order in articles.keys():
+                shopping_cart.append(order)
+                for key in shopping_cart:
+                    pass
+                current_order.append(articles.get(key)[0])
+                print(f"Current order: {current_order}")
+            elif order == complete:
+                print("Thank you for choosing us!")
+                for order in shopping_cart:
+                    pass    # print values der bestellung ? neues dict??
+                print(f"Your order: {current_order}", end=",")
+                active_shop = False
+            elif order == exit:
                 print("See you next time.")
                 shopping_cart.clear()
                 active_shop = False
-            elif order == complete.lower():
-                print("Thank you for choosing us!")
-                print(f"Your order: {shopping_cart}")
-                active_shop = False
-            elif int(order) in articles.keys():
-                shopping_cart.append(int(order))
-                print(f"Current order: {shopping_cart}")
-            elif int(order) not in articles.keys():
-                print("Invalid input.")
-            elif order not in articles.values():
-                print("Invalid input.")
             else:
                 print("Invalid input.")
+
+
 
 
 shopping.available()
